4be39dad69ba67dd6bdb72b276605e6e4abb5cce
INKYU <INKYU@DESKTOP-N79JQSM>
2020-02-15 22:02:05 +0900

    commit3

--- SpringB/src/main/java/com/spring/board/controller/HelloController.java
+++ SpringB/src/main/java/com/spring/board/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
+		
 		return "/WEB-INF/views/hello.jsp";
 	}
 }
